a62e68bee2728476021c41294d12bb38a36e14b6
jarogu90 <jaime.romo@apptivatech.com>
2020-04-07 18:00:22 +0200

    config.json pruebas tabla resultados

--- busqueda_app/src/components/Samples.js
+++ busqueda_app/src/components/Samples.js
@@ -49,36 +49,81 @@ const OrderHitsListItem = (props)=> {
 
 const OrderHitsTable = (props) => {  
   const {hits} = props;
+  return (
+    <div style={{width: '100%', boxSizing: 'border-box', padding: 8}}>
+      <table className="sk-table sk-table-striped" style={{width: '100%', boxSizing: 'border-box'}}>
+        <thead>
+          <tr>
+            <th>Actual Delivery Date</th>
+            <th>ORDER_ID</th>
+            <th>City</th>
+            <th>Shipping Point</th>
+            <th>Delivery Type</th>
+            <th>Contact Person Signature</th>
+            <th>Commercial Distance</th>
+            <th>Transport Distance</th>
+            <th>Transport Duration Minutes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {hits.map((hit) => {
+            return (
+              <tr key={hit._id}>
+                <td>{hit._source.ACTUAL_DELIVERY_DAT}</td>
+                <td>{hit._source.ORDER_ID}</td>
+                <td>{hit._source.LNF_SITE_CITY}</td>
+                <td>{hit._source.SHIPPINGPOINT_ID}</td>
+                <td>{hit._source.DELIVERY_TYPE_CD}</td>
+                <td>{hit._source.CONTACT_PERSON_SIGNATURE_TXT}</td>
+                <td>{hit._source.COMMERCIAL_DISTANCE_M}</td>
+                <td>{hit._source.TRANSPORT_DISTANCE_M}</td>
+                <td>{hit._source.TRANSPORT_DURATION_MIN}</td>
+              </tr>
+            )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )  
+  }
+/*const OrderHitsTable = (props) => {  
+  const {hits} = props;
   const fields = config.samples.table.fields;
+  const table = []
+  const body = fields.map((f) => {
+    hits.map((hit) => {
+      table.push(<td key={hit._id}>{hit._source[f]}</td>)
+    })
+    console.log(table)
+    return(
+      <tr>
+        {table}
+      </tr>
+    );
+  })
 
   return (  
     <div style={{width: '100%', boxSizing: 'border-box', padding: 8}}>
-      {fields.map((field, index) => {
-        return(
-          <table className="sk-table sk-table-striped" style={{width: '100%', boxSizing: 'border-box'}}>
-            <thead>
-              <tr>
-                <th key={index}>{field}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                hits.map((hit) => {
-                  return(
-                    <tr key={hit._id}>
-                      <td>{hit._source[field]}</td>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
-          </table> 
-        )
-      })
-      }
+      <table className="sk-table sk-table-striped" style={{width: '100%', boxSizing: 'border-box'}}>
+        <thead>
+          <tr>
+            {
+              fields.map((field, index) => {
+                return(<th key={index}>{field}</th>)
+              })
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {
+            body
+          }
+        </tbody>
+      </table> 
     </div>
   )  
-}
+}*/
 
 class Samples extends SearchkitComponent {
   render(){
