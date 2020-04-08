a84a671993069cdef83b934d5971e37517ab150f
jarogu90 <jaime.romo@apptivatech.com>
2020-04-08 09:21:55 +0200

    config.json tabla de resultados arreglada

--- busqueda_app/src/components/Samples.js
+++ busqueda_app/src/components/Samples.js
@@ -16,11 +16,11 @@ const OrderHitsGridItem = (props) => {
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))}>
       <ul>
         <h3>Order Details:</h3>
-        {fields.map((field, index) => {
-          return(
-            <li key={index} className={bemBlocks.item(field)}>{field}: {source[field]}</li>
-          )
-        })}
+          {fields.map((field, index) => {
+            return(
+              <li key={index} className={bemBlocks.item(field)}>{field}: {source[field]}</li>
+            )
+          })}
       </ul>
     </div>
   )
@@ -49,61 +49,9 @@ const OrderHitsListItem = (props)=> {
 
 const OrderHitsTable = (props) => {  
   const {hits} = props;
-  return (
-    <div style={{width: '100%', boxSizing: 'border-box', padding: 8}}>
-      <table className="sk-table sk-table-striped" style={{width: '100%', boxSizing: 'border-box'}}>
-        <thead>
-          <tr>
-            <th>Actual Delivery Date</th>
-            <th>ORDER_ID</th>
-            <th>City</th>
-            <th>Shipping Point</th>
-            <th>Delivery Type</th>
-            <th>Contact Person Signature</th>
-            <th>Commercial Distance</th>
-            <th>Transport Distance</th>
-            <th>Transport Duration Minutes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {hits.map((hit) => {
-            return (
-              <tr key={hit._id}>
-                <td>{hit._source.ACTUAL_DELIVERY_DAT}</td>
-                <td>{hit._source.ORDER_ID}</td>
-                <td>{hit._source.LNF_SITE_CITY}</td>
-                <td>{hit._source.SHIPPINGPOINT_ID}</td>
-                <td>{hit._source.DELIVERY_TYPE_CD}</td>
-                <td>{hit._source.CONTACT_PERSON_SIGNATURE_TXT}</td>
-                <td>{hit._source.COMMERCIAL_DISTANCE_M}</td>
-                <td>{hit._source.TRANSPORT_DISTANCE_M}</td>
-                <td>{hit._source.TRANSPORT_DURATION_MIN}</td>
-              </tr>
-            )
-            })
-          }
-        </tbody>
-      </table>
-    </div>
-  )  
-  }
-/*const OrderHitsTable = (props) => {  
-  const {hits} = props;
   const fields = config.samples.table.fields;
-  const table = []
-  const body = fields.map((f) => {
-    hits.map((hit) => {
-      table.push(<td key={hit._id}>{hit._source[f]}</td>)
-    })
-    console.log(table)
-    return(
-      <tr>
-        {table}
-      </tr>
-    );
-  })
 
-  return (  
+  return (
     <div style={{width: '100%', boxSizing: 'border-box', padding: 8}}>
       <table className="sk-table sk-table-striped" style={{width: '100%', boxSizing: 'border-box'}}>
         <thead>
@@ -117,13 +65,25 @@ const OrderHitsTable = (props) => {
         </thead>
         <tbody>
           {
-            body
+            hits.map((hit) => {
+              return (
+                <tr key={hit._id}>
+                  {
+                    fields.map((f) => {
+                      return(                   
+                        <td>{hit._source[f]}</td>                   
+                      )
+                    })
+                  }
+                </tr>
+              )
+            })
           }
         </tbody>
-      </table> 
+      </table>
     </div>
   )  
-}*/
+}
 
 class Samples extends SearchkitComponent {
   render(){
