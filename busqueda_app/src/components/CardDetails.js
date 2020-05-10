1e4653a54203a56f8e270b25a69519f16326483b
jarogu90 <jaime.romo@apptivatech.com>
2020-05-10 12:52:09 +0200

    tabla details vertical

--- busqueda_app/src/components/CardDetails.js
+++ busqueda_app/src/components/CardDetails.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import { useParams, Link } from 'react-router-dom';
+import config from "../config.json";
 
 function CardDetails() {
     let {order_id} = useParams();
-
     const [item, setItem] = useState({});
 
     useEffect(() => {
@@ -27,36 +27,65 @@ function CardDetails() {
 
     return(
         <div className="details-wrapper">
-            <div className="details-header">
-                <div className="details-title">Order Details</div>
+            <div className="details-header">         
+                <div className="details-title">{config.title}</div>
             </div>
             <div className="details-content">
-            {
-                (!item.newItem) ? (<div>Loading...</div>) : (                 
-                    <table className="details-table">
-                        <thead>
-                            <tr className="details-table-header">
-                                <th>ORDER ID</th>
-                                <th>ORDER ITEM ID</th>
-                                <th>ORDERED QUANTITY</th>
-                                <th>DELIVERED QUANTITY</th>
-                                <th>CURRENCY CD</th>
-                                <th>GROSS PRICE MAT</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className="details-table-content">
-                                <td>{item.newItem.ORDER_ID}</td>
-                                <td>{item.newItem.ORDER_ITEM_ID}</td>
-                                <td>{item.newItem.ORDERED_QUANTITY}</td>
-                                <td>{item.newItem.DELIVERED_QUANTITY}</td>
-                                <td>{item.newItem.CURRENCY_CD}</td>
-                                <td>{item.newItem.GROSS_PRICE_MAT}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                )   
-            }
+                <h2 className="details-table-title">Order Details:</h2>
+                {
+                    (!item.newItem) ? (<div className="loading">Loading...</div>) : (                                       
+                        <div className="details-table">
+                            <div className="details-table-row">
+                                <div className="details-data">
+                                    <div className="details-data-position">ORDER ID</div>
+                                </div>
+                                <div className="details-data-content">
+                                    <div className="details-data-position">{item.newItem.ORDER_ID}</div>
+                                </div>
+                            </div>
+                            <div className="details-table-row">
+                                <div className="details-data">
+                                    <div className="details-data-position">ORDER ITEM ID</div>
+                                </div>
+                                <div className="details-data-content">
+                                    <div className="details-data-position">{item.newItem.ORDER_ITEM_ID}</div>
+                                </div>
+                            </div>  
+                            <div className="details-table-row">
+                                <div className="details-data">
+                                    <div className="details-data-position">ORDERED QUANTITY</div>
+                                </div>
+                                <div className="details-data-content">
+                                    <div className="details-data-position">{item.newItem.ORDERED_QUANTITY}</div>
+                                </div>
+                            </div>  
+                            <div className="details-table-row">
+                                <div className="details-data">
+                                    <div className="details-data-position">DELIVERED QUANTITY</div>
+                                </div>
+                                <div className="details-data-content">
+                                    <div className="details-data-position">{item.newItem.DELIVERED_QUANTITY}</div>
+                                </div>
+                            </div>  
+                            <div className="details-table-row">
+                                <div className="details-data">
+                                    <div className="details-data-position">CURRENCY CD</div>
+                                </div>
+                                <div className="details-data-content">
+                                    <div className="details-data-position">{item.newItem.CURRENCY_CD}</div>
+                                </div>
+                            </div>  
+                            <div className="details-table-row">
+                                <div className="details-data">
+                                    <div className="details-data-position">GROSS PRICE MAT</div>
+                                </div>
+                                <div className="details-data-content">
+                                    <div className="details-data-position">{item.newItem.GROSS_PRICE_MAT}</div>
+                                </div>
+                            </div>  
+                        </div>            
+                    )   
+                }
                 <Link to="/" className="button-return">
                     Back
                 </Link>
