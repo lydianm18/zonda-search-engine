962417abfa77b2033ef6bd1f32873ad359f2b6b7
jarogu90 <jaime.romo@apptivatech.com>
2020-05-11 14:53:15 +0200

    popup desde la vista Table

--- busqueda_app/src/components/Samples.js
+++ busqueda_app/src/components/Samples.js
@@ -151,7 +151,11 @@ const OrderHitsTable = (props) => {
 
               return (
                 <tr key={hit._id} className="table-header">
-                  <td>{hit._source.ORDER_ID}</td>
+                  <td>
+                    <Popup trigger={<div>{hit._source.ORDER_ID}</div>} modal>
+                      <CardDetails order_id={hit._source.ORDER_ID} />
+                    </Popup>
+                  </td>              
                   <td>{hit._source.ACTUAL_DELIVERY_DAT}</td>              
                   <td>{hit._source.SHIPPINGPOINT_ID}</td>
                   <td><DeliveryType /></td>
