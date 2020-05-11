9646a5acd1d078c7233a128b1106069667af1ace
jarogu90 <jaime.romo@apptivatech.com>
2020-05-11 11:36:48 +0200

    detalles del pedido formato popup

--- busqueda_app/src/components/CardDetails.js
+++ busqueda_app/src/components/CardDetails.js
@@ -1,15 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import { useParams, Link } from 'react-router-dom';
+//import { useParams, Link } from 'react-router-dom';
+import Samples from './Samples';
 import config from "../config.json";
 
 function CardDetails() {
-    let {order_id} = useParams();
+
     const [item, setItem] = useState({});
 
     useEffect(() => {
         const getData = () => {
             try{
-                fetch(`https://search-logon-order-search-qyhfl5jwmhiaatf23g3wafmjia.eu-west-1.es.amazonaws.com/tlgnc_order_item/_search?q=ORDER_ID:${order_id}`)
+                fetch(`https://search-logon-order-search-qyhfl5jwmhiaatf23g3wafmjia.eu-west-1.es.amazonaws.com/tlgnc_order_item/_search?q=ORDER_ID:`)
                 .then(response => response.json())
                 .then(result => {
                     let newItem = result.hits.hits[0]._source;
@@ -86,9 +87,6 @@ function CardDetails() {
                         </div>            
                     )   
                 }
-                <Link to="/" className="button-return">
-                    Back
-                </Link>
             </div>
         </div>
     )
