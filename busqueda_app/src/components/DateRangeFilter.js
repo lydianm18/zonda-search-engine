19d3568e09ba50db3e7bbe9ba00c530423e3f493
jarogu90 <jaime.romo@apptivatech.com>
2020-09-25 09:56:35 +0200

    cambio formato calendario, falta mostrar cambio de año en pantalla

--- busqueda_app/src/components/DateRangeFilter.js
+++ busqueda_app/src/components/DateRangeFilter.js
@@ -10,8 +10,32 @@ class DateRangeFilter extends Component {
     super(props)
     this.state = {
       startDate: null,
-      endDate: null
+      endDate: null,
+      years: this.range(1990, new Date().getFullYear()),
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ]
     }
+  }
+
+  range = (startYear, endYear) => {
+    let arrayYears = []
+    while(startYear <= endYear){
+      arrayYears.push(startYear)
+      startYear += 1
+    }
+    return arrayYears;
   }
 
   handleChangeStart = (event) => {
@@ -74,7 +98,56 @@ class DateRangeFilter extends Component {
               selected={this.state.startDate}
               startDate={this.state.startDate}
               endDate={this.state.endDate}
-              onChange={this.handleChangeStart} />
+              onChange={this.handleChangeStart} 
+              renderCustomHeader={({
+                date,
+                changeYear,
+                changeMonth,
+                decreaseMonth,
+                increaseMonth,
+                prevMonthButtonDisabled,
+                nextMonthButtonDisabled
+              }) => (
+                <div
+                  style={{
+                    margin: 10,
+                    display: "flex",
+                    justifyContent: "center"
+                  }}
+                >
+                  <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+                    {"<"}
+                  </button>
+                  <select
+                    value={date.getFullYear}
+                    onChange={({ target: { value } }) => changeYear(value)}
+                  >
+                    {this.state.years.map(option => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+
+                  <select
+                    value={this.state.months[date.getMonth]}
+                    onChange={({ target: { value } }) =>
+                      changeMonth(this.state.months.indexOf(value))
+                    }
+                  >
+                    {this.state.months.map(option => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+
+                  <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+                    {">"}
+                  </button>
+                </div>
+              )}
+      />
             </form>
           </div>
         </div>
