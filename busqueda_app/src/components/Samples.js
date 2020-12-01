78a5e36397c254218c018480ce8b5f9444a7d520
jarogu90 <jaime.romo@apptivatech.com>
2020-12-01 14:07:35 +0100

    cambio de componente Samples de clase a const

--- busqueda_app/src/components/Samples.js
+++ busqueda_app/src/components/Samples.js
@@ -7,39 +7,24 @@ import {
 import OrderHitsTable from './OrderHitsTable';
 import config from "../config.json";
 
-class Samples extends SearchkitComponent {
-
-  /*state = {
-    datos: []
-  }
-
-  componentDidUpdate(prevProps) {
-    //console.log(this.props, prevProps) 
-    if(prevProps.dateFilterOn !== this.props.dateFilterOn){
-      console.log('distinto')
-      this.setState({datos: this.props.dataDateFilter})
-    }
-  }*/
-
-  render(){
-    console.log('SAMPLES', this.props.dataDateFilter)
-    return (
-      <div>
-          <Hits
+const Samples = ({dataDateFilter}) => {
+  return (
+    <div>
+      <Hits
             hitsPerPage={1000}
             highlightFields={["ORDER_ID"]}
-            listComponent= {<OrderHitsTable dataDateFilter={this.props.dataDateFilter}/>}
+            listComponent= {<OrderHitsTable dataDateFilter={dataDateFilter}/>}
             hitComponents={[
               {key: config.samples.table.key, title: config.samples.table.title}]}
             scrollTo="body"
           />
           <NoHits />
-      </div> 
-    )
-  } 
+    </div>
+  )
 }
 
-export default Samples;
+export default Samples
+
 
 
   
